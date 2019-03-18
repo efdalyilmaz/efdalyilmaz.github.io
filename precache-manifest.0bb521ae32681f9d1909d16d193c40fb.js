f998fb75f1ab603f3ebea940e7a29313f4ff7191
eyilmaz <eyilmaz@ziraatteknoloji.com>
2019-03-18 20:55:38 +0300

    memory cards

---
+++ precache-manifest.0bb521ae32681f9d1909d16d193c40fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8c616b064d83d55f8441",
+    "url": "/static/js/main.79f714ba.chunk.js"
+  },
+  {
+    "revision": "7d05e7bd9822020913ac",
+    "url": "/static/js/2.7e07be04.chunk.js"
+  },
+  {
+    "revision": "8c616b064d83d55f8441",
+    "url": "/static/css/main.a9496b67.chunk.css"
+  },
+  {
+    "revision": "a66a6e1efa1e816a6d46f0265f149eb6",
+    "url": "/index.html"
+  }
+];
